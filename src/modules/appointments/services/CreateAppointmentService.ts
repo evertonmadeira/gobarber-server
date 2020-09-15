3718a8edf76ba5423a99aabd41827570185bc863
Everton Madeira <everton.comp@gmail.com>
2020-09-15 20:42:59 -0300

    Create Appointment Service and its test finished

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { isBefore, startOfHour, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointments from '@modules/appointments/infra/typeorm/entities/Appointments';
@@ -25,12 +25,26 @@ export default class CreateAppointmentService {
   }: IRequestDTO): Promise<Appointments> {
     const appointmentDate = startOfHour(date);
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm',
+      );
+    }
+
     const findAppointmentIsSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
     if (findAppointmentIsSameDate) {
-      throw new AppError('This appointment is already booked!', 400);
+      throw new AppError('This appointment is already booked!');
     }
 
     const appointment = await this.appointmentsRepository.create({

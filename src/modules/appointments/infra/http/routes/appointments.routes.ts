f80f63764c6bca02fa046be016d2c4ba1a157012
Everton Madeira <everton.comp@gmail.com>
2020-09-16 23:13:00 -0300

    Has been created list provider schedule

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentRouter = Router();
 
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
-// appointmentRouter.get('/', async (request, response) => {
-
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 appointmentRouter.post('/', appointmentsController.create);
 
---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,36 @@
+import { injectable, inject } from 'tsyringe';
+import Appointments from '../infra/typeorm/entities/Appointments';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequestDTO {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+export default class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequestDTO): Promise<Appointments[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
